c8f565ebdabd805fcdc524b66f0f8ecc31974022
potemkin <86324875+potemkin1309@users.noreply.github.com>
2021-11-15 00:43:29 +0300

    Add files via upload

---
+++ testCat.py
@@ -0,0 +1,12 @@
+from cat import Cat
+
+c1 = Cat("Baron", "male", 2)
+c2 = Cat("Sam", "male", 2)
+
+print("c1.name = ", c1.name)
+print("c1.gender = ", c1.gender)
+print("c1.age = ", c1.age)
+print("***")
+print("c2.name = ", c2.name)
+print("c2.gender = ", c2.gender)
+print("c2.age = ", c2.age)
