14bc156e5d12aac0d50d35b3ed19c567e0ad8c39
potemkin <86324875+potemkin1309@users.noreply.github.com>
2021-11-15 23:08:10 +0300

    Задание 16.8.2

---
+++ rectangle_2.py
@@ -0,0 +1,32 @@
+from rectangle import Rectangle, Square, Circle
+
+# далее создаем два прямоугольника
+
+rect_1 = Rectangle(3,4)
+rect_2 = Rectangle(12,5)
+
+# вывод площади наших двух прямогулоьников
+print(rect_1.get_area())
+print(rect_2.get_area())
+
+# далее создаем два квадрата
+square_1 = Square(5)
+square_2 = Square(10)
+# вывод площади наших двух квадратов
+print(square_1.get_area_square(),
+      square_2.get_area_square())
+
+# создаем круг
+circle_1 = Circle(3)
+# вывод площади круга
+print(circle_1.get_area_circle())
+
+print("***")
+
+figures = [rect_1, rect_2, square_1, square_2]
+for figure in figures:
+    if isinstance(figure,Square):
+        print(figure.get_area_square())
+    else:
+        print(figure.get_area())
+
